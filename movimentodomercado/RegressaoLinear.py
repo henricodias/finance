e8ac699eaa949cc90a6f2e84e286b54625b0b5d2
henricodias <henrico.diass@gmail.com>
2022-04-02 19:36:48 -0300

    Correlacao entre indicadores

--- movimentodomercado/RegressaoLinear.py
+++ movimentodomercado/RegressaoLinear.py
@@ -26,23 +26,58 @@ df['time'] = pd.to_datetime(df['time'], unit='s')
 #print(df)
 
 df['close'].plot(label="PETR4", legend=True)
-plt.show()
+#plt.show()
 plt.clf()
 
 #plota retorno em forma de histograma
 df['close'].pct_change().plot.hist(bins=50)
-plt.show()
+#plt.show()
 plt.clf()
 
-pacoteDeDias = 5
+pacoteDeDias = 15
 df['dias_no_futuro'] = df['close'].shift(pacoteDeDias)
 df['dias_no_futuro_retorno'] = df['dias_no_futuro'].pct_change(pacoteDeDias)
 df['dias_atuais_retorno'] = df['close'].pct_change(pacoteDeDias)
-print(df.tail(10))
+#print(df.tail(10))
 
 correlacao = df[['dias_atuais_retorno', 'dias_no_futuro_retorno']].corr()
-print("Correção: " +str(correlacao))
+#print("Correção: " + str(correlacao))
 
 plt.scatter(df['dias_atuais_retorno'],df['dias_no_futuro_retorno'])
+#plt.show()
+#plt.clf()
+
+#criação de indicadores
+df['obv'] = talib.OBV(df['close'], df['real_volume'])
+featureNames = ['dias_atuais_retorno', 'obv']
+
+for n in [2,7,9,21,50,200]:
+    #criar média móvel exponencial
+    df['mme' + str(n)] = talib.EMA(df['close'].values, timeperiod=n)
+
+    #criar rsi
+    df['rsi' + str(n)] = talib.RSI(df['close'].values, timeperiod=n)
+
+    featureNames = featureNames + ['mme' + str(n), 'rsi' + str(n)]
+
+#print(featureNames)
+#print(df)
+
+df = df.dropna()
+
+features = df[featureNames]
+target = df['dias_no_futuro_retorno']
+
+featureAndTarget = ['dias_no_futuro_retorno'] + featureNames
+featureAndTargetDf = df[featureAndTarget]
+
+correlacionado = featureAndTargetDf.corr()
+print(correlacionado)
+
+
+sns.heatmap(correlacionado, annot=True, annot_kws={"size": 5})
+plt.yticks(rotation=0, size=8)
+plt.xticks(rotation=90, size=8)
+plt.tight_layout()
 plt.show()
 plt.clf()
