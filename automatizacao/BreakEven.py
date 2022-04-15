50bbd3554e83ec8d5df8618040e586e3a04f5063
henricodias <henrico.diass@gmail.com>
2022-04-15 15:59:32 -0300

    Definicao da ordem de compra

--- automatizacao/BreakEven.py
+++ automatizacao/BreakEven.py
@@ -99,3 +99,29 @@ def venda():
     resultado = mt5.order_send(request)
     return resultado
 
+#define a ordem de compra:
+def compra():
+    print("ORDEM DE COMPRA ENVIADA")
+    lot = float(1)
+    symbol = ativo
+    point = mt5.symbol_info(symbol).point
+    price = mt5.symbol_info_tick(symbol).last
+    deviation = 5
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": lot,
+        "type": mt5.ORDER_TYPE_BUY,
+        "price": price,
+        "sl": price - stopLoss * point,
+        "tp": price + takeProfit * point,
+        "deviation": deviation,
+        "magic": 10032021,
+        "comment": "Ordem de Compra Enviada",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_RETURN
+    }
+    resultado = mt5.order_send(request)
+    return resultado
+
+
